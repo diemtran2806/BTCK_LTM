b25566375e107049e6180a10aaa60cbdcea48e15
diemtran2806 <ttdiem2002@gmail.com>
2022-11-27 16:00:52 +0700

    connectdb file

---
+++ src/main/java/model/BEAN/Class.java
@@ -0,0 +1,26 @@
+package model.BEAN;
+
+public class Class {
+	private int id_class;
+	private String class_name;
+	private int id_faculty;
+	public int getId_class() {
+		return id_class;
+	}
+	public void setId_class(int id_class) {
+		this.id_class = id_class;
+	}
+	public String getClass_name() {
+		return class_name;
+	}
+	public void setClass_name(String class_name) {
+		this.class_name = class_name;
+	}
+	public int getId_faculty() {
+		return id_faculty;
+	}
+	public void setId_faculty(int id_faculty) {
+		this.id_faculty = id_faculty;
+	}
+
+}
---
+++ src/main/java/model/DAO/ConnectDatabase.java
@@ -0,0 +1,26 @@
+package model.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ConnectDatabase {
+	
+	public static Connection getMySQLConnection() throws ClassNotFoundException, SQLException
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/qlsv", "root", "");
+		if (conn != null) {
+			System.out.println("Connected!");
+			return conn;
+		}
+		return null;
+	}
+//	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//		ConnectDatabase t = new ConnectDatabase();
+//		Connection con = t.getMySQLConnection();
+//	}
+}
